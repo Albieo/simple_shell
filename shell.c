bc6982b29516a4c92fe908c94eac74bb11d4ce23
Albert Mpepo <79055666+Albieo@users.noreply.github.com>
2023-07-19 12:05:03 +0200

    Final update on the base shell 1.0

--- shell.c
+++ shell.c
@@ -1,40 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define MAX_COMMAND_LENGTH 1024
 
 int main(void)
 {
 	char command[MAX_COMMAND_LENGTH];
+	char *args[MAX_COMMAND_LENGTH / 2];
 	ssize_t num_chars_read;
-	pid_t pid;
 	int status;
+	int arg_count;
+	pid_t pid;
 
 	while (1)
 	{
-		write(STDOUT_FILENO, "#cisfun$ ", 13);
+		write(1, ":) ", 3);
+		fflush(stdout);
+		num_chars_read = read(0, command, MAX_COMMAND_LENGTH);
 
-		num_chars_read = read(STDIN_FILENO, command, MAX_COMMAND_LENGTH);
 		if (num_chars_read == -1)
+		{
+			perror("Error reading input");
 			break;
+		}
+
+		if (num_chars_read == 0)
+		{
+			write(1, "\n", 1);
+			break;
+		}
 
 		command[num_chars_read - 1] = '\0';
+
+		arg_count = 0;
+		args[arg_count] = strtok(command, " ");
+		while (args[arg_count] != NULL && arg_count < MAX_COMMAND_LENGTH / 2)
+		{
+			arg_count++;
+			args[arg_count] = strtok(NULL, " ");
+		}
+		args[arg_count] = NULL;
+
+		if (strcmp(args[0], "exit") == 0)
+		{
+            		break;
+		}
 
 		pid = fork();
 
 		if (pid < 0)
+		{
+			perror("Fork failed");
 			break;
-
+		}
 		else if (pid == 0)
 		{
-			char **argv = (char **)malloc(2 * sizeof(char *));
-			argv[0] = command;
-			argv[1] = NULL;
-			execve(command, argv, NULL);
-
-			free(argv);
+			execvp(args[0], args);
+			perror("Command execution failed");
 			_exit(EXIT_FAILURE);
 		}
 		else
